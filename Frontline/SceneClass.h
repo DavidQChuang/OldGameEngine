b9d4363bde0e84aa61400fd0cab9327cd38865eb
David Chuang <dc1770700@gmail.com>
2018-10-24 06:37:05 -0400

    Fixed inline functions

--- Frontline/SceneClass.h
+++ Frontline/SceneClass.h
@@ -16,9 +16,9 @@ public:
 	SceneClass(HWND hwnd, D3DClass*, CameraClass*, ShaderClass*);
 	SceneClass(const SceneClass&);
 	~SceneClass();
-	virtual bool Initialize()=0;
-	virtual void Shutdown()=0;
-	virtual bool Render(XMMATRIX, XMMATRIX, XMMATRIX)=0;
+	virtual bool Initialize() = 0;
+	virtual void Shutdown() = 0;
+	virtual bool Render(XMMATRIX, XMMATRIX, XMMATRIX) = 0;
 	bool m_active;
 
 	inline bool RenderRectMatrix(TexturedRect*, XMMATRIX, XMMATRIX, XMMATRIX, int);
@@ -30,16 +30,14 @@ public:
 	inline bool RenderRect(TexturedRect*, int, int, float, XMMATRIX, XMMATRIX, int);
 	inline bool RenderRect(TexturedRect*, int, int, float, float, XMMATRIX, XMMATRIX, int);
 	inline bool RenderRect(TexturedRect*, int, int, float, float, float, XMMATRIX, XMMATRIX, int);
-	//int x, int y, [float rot], [int spritenumber]
-	//after matrices, shader info
+
 	inline bool RenderSpritesheetMatrix(TexturedSpritesheet*, XMMATRIX, XMMATRIX, XMMATRIX, int);
 	inline bool RenderSpritesheet(TexturedSpritesheet*, int, int, XMMATRIX, XMMATRIX, int);
 	inline bool RenderSpritesheet(TexturedSpritesheet*, int, int, int, XMMATRIX, XMMATRIX, int);
 	inline bool RenderSpritesheet(TexturedSpritesheet*, int, int, float, XMMATRIX, XMMATRIX, int);
 	inline bool RenderSpritesheet(TexturedSpritesheet*, int, int, float, int, XMMATRIX, XMMATRIX, int);
-	//bool RenderSpritesheet(TexturedSpritesheet*, int, int, float, int, XMMATRIX, XMMATRIX, int);
 
-	bool RenderModel(ModelClass*, XMMATRIX, XMMATRIX, int);
+	inline bool RenderModel(ModelClass*, XMMATRIX, XMMATRIX, int);
 private:
 protected:
 	HWND sm_hwnd;
@@ -48,3 +46,225 @@ protected:
 	ShaderClass* sm_ShaderClass;
 	Timer* sm_Timer;
 };
+
+//  
+//  Renders a TexturedRect from the buffer using a shader and passing a translation matrix as the world matrix.
+//  
+inline bool SceneClass::RenderRectMatrix(TexturedRect* object, XMMATRIX matrix, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	switch (shaderType) {
+	case H_2D_COLOR_SHADERTYPE:
+		return sm_ShaderClass->m_ColorShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix);
+		break;
+	case H_2D_TEXTURE_SHADERTYPE:
+		return sm_ShaderClass->m_TextureShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture());
+		break;
+	case H_2D_COLOR_TEXTURE_SHADERTYPE:
+		return sm_ShaderClass->m_ColorTextureShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture());
+		break;
+	case H_2D_COLOR_SHADERTYPE | H_2D_TRANSPARENCY_SHADERMOD:
+		//return sm_ShaderClass->m_TransparencyShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture(), 1.f);
+		break;
+	case H_2D_TEXTURE_SHADERTYPE | H_2D_TRANSPARENCY_SHADERMOD:
+		return sm_ShaderClass->m_TransparencyShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture(), 1.f);
+		break;
+	case H_2D_COLOR_TEXTURE_SHADERTYPE | H_2D_TRANSPARENCY_SHADERMOD:
+		//return sm_ShaderClass->m_TransparencyShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture(), 1.f);
+		break;
+	case H_2D_COLOR_SHADERTYPE | H_2D_INSTANCE_SHADERMOD:
+		break;
+	case H_2D_TEXTURE_SHADERTYPE | H_2D_INSTANCE_SHADERMOD:
+		break;
+	case H_2D_COLOR_TEXTURE_SHADERTYPE | H_2D_INSTANCE_SHADERMOD:
+		break;
+	}
+	return false;
+}
+
+//
+//  Abstraction for writing a TexturedRect to buffer. 
+//  Uses TexturedRect.m_posX & m_posY
+//
+inline bool SceneClass::RenderRect(TexturedRect* object, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix = DirectX::XMMatrixTranslation(object->m_posX - 400 + object->m_imageWidth / 2, -object->m_posY + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext());
+	RenderRectMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+
+//
+//  Abstraction for writing a TexturedRect to buffer. 
+//  float rot: Clockwise rotation in degrees
+//
+inline bool SceneClass::RenderRect(TexturedRect* object, float rot, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix =
+		DirectX::XMMatrixRotationZ(XMConvertToRadians(rot)) *
+		DirectX::XMMatrixTranslation(object->m_posX - 400 + object->m_imageWidth / 2, -object->m_posY + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext());
+	RenderRectMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+
+//
+//  Abstraction for writing a TexturedRect to buffer. 
+//  float scaleX, float scaleY: Scale multiplier
+//
+inline bool SceneClass::RenderRect(TexturedRect* object, float scaleX, float scaleY, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix =
+		DirectX::XMMatrixScaling(scaleX, scaleY, 1.0f) *
+		DirectX::XMMatrixTranslation(object->m_posX - 400 + object->m_imageWidth / 2, -object->m_posY + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext());
+	RenderRectMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+//
+//  Abstraction for writing a TexturedRect to buffer. 
+//  float scaleX, float scaleY: Scale multiplier
+//  float rot: Clockwise rotation in degrees
+//
+inline bool SceneClass::RenderRect(TexturedRect* object, float scaleX, float scaleY, float rot, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix =
+		DirectX::XMMatrixScaling(scaleX, scaleY, 1.0f) *
+		DirectX::XMMatrixRotationZ((XMConvertToRadians(rot))) *
+		DirectX::XMMatrixTranslation(object->m_posX - 400 + object->m_imageWidth / 2, -object->m_posY + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext());
+	RenderRectMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+//
+//  Abstraction for writing a TexturedRect to buffer. 
+//  int x, int y: X and Y coords (self explanatory) 
+//
+inline bool SceneClass::RenderRect(TexturedRect* object, int x, int y, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix =
+		DirectX::XMMatrixTranslation(x - 400 + object->m_imageWidth / 2, -y + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext(), x, y);
+	return RenderRectMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+//
+//  Abstraction for writing a TexturedRect to buffer. 
+//  int x, int y: X and Y coords (self explanatory) 
+//  float rot: Clockwise rotation in degrees
+//
+inline bool SceneClass::RenderRect(TexturedRect* object, int x, int y, float rot, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix =
+		DirectX::XMMatrixRotationZ(XMConvertToRadians(rot)) *
+		DirectX::XMMatrixTranslation(x - 400 + object->m_imageWidth / 2, -y + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext(), x, y);
+	RenderRectMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+//
+//  Abstraction for writing a TexturedRect to buffer. 
+//  int x, int y: X and Y coords (self explanatory) 
+//  float scaleX, float scaleY: Scale multiplier
+//
+inline bool SceneClass::RenderRect(TexturedRect* object, int x, int y, float scaleX, float scaleY, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix =
+		DirectX::XMMatrixScaling(scaleX, scaleY, 1.0f) *
+		DirectX::XMMatrixTranslation(x - 400 + object->m_imageWidth / 2, -y + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext(), x, y);
+	RenderRectMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+//
+//  Abstraction for writing a TexturedRect to buffer. 
+//  int x, int y: X and Y coords (self explanatory) 
+//  float scaleX, float scaleY: Scale multiplier
+//  float rot: Clockwise rotation in degrees
+//
+inline bool SceneClass::RenderRect(TexturedRect* object, int x, int y, float scaleX, float scaleY, float rot, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix =
+		DirectX::XMMatrixScaling(scaleX, scaleY, 1.0f) *
+		DirectX::XMMatrixRotationZ(XMConvertToRadians(rot)) *
+		DirectX::XMMatrixTranslation(x - 400 + object->m_imageWidth / 2, -y + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext(), x, y);
+	RenderRectMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+
+//
+//  Renders a TexturedRect from the buffer using a shader and passing a translation matrix as the world matrix.
+//
+inline bool SceneClass::RenderSpritesheetMatrix(TexturedSpritesheet* object, XMMATRIX matrix, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	switch (shaderType) {
+	case H_2D_COLOR_SHADERTYPE:
+		return sm_ShaderClass->m_ColorShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix);
+		break;
+	case H_2D_TEXTURE_SHADERTYPE:
+		return sm_ShaderClass->m_TextureShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture());
+		break;
+	case H_2D_COLOR_TEXTURE_SHADERTYPE:
+		return sm_ShaderClass->m_ColorTextureShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture());
+		break;
+	case H_2D_COLOR_SHADERTYPE | H_2D_TRANSPARENCY_SHADERMOD:
+		//return sm_ShaderClass->m_TransparencyShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture(), 1.f);
+		break;
+	case H_2D_TEXTURE_SHADERTYPE | H_2D_TRANSPARENCY_SHADERMOD:
+		return sm_ShaderClass->m_TransparencyShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture(), 1.f);
+		break;
+	case H_2D_COLOR_TEXTURE_SHADERTYPE | H_2D_TRANSPARENCY_SHADERMOD:
+		//return sm_ShaderClass->m_TransparencyShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture(), 1.f);
+		break;
+	case H_2D_COLOR_SHADERTYPE | H_2D_INSTANCE_SHADERMOD:
+		break;
+	case H_2D_TEXTURE_SHADERTYPE | H_2D_INSTANCE_SHADERMOD:
+		break;
+	case H_2D_COLOR_TEXTURE_SHADERTYPE | H_2D_INSTANCE_SHADERMOD:
+		break;
+	}
+	return false;
+}
+//
+//  Abstraction for writing a TexturedSpritesheet to buffer. 
+//  int x, int y: X and Y coords (self explanatory)
+//
+inline bool SceneClass::RenderSpritesheet(TexturedSpritesheet* object, int x, int y, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix = DirectX::XMMatrixTranslation(x - 400 + object->m_spriteWidth / 2, -y + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext(), x, y);
+	RenderSpritesheetMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+
+//
+//  Abstraction for writing a TexturedSpritesheet to buffer.  
+//  int x, int y: X and Y coords (self explanatory) 
+//  int sprite: Sprite number (calls TexturedSpritesheet::SetSprite(sprite))
+//
+inline bool SceneClass::RenderSpritesheet(TexturedSpritesheet* object, int x, int y, int sprite, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix = DirectX::XMMatrixTranslation(x - 400 + object->m_spriteWidth / 2, -y + 300 - object->m_imageHeight / 2, 0.f);
+	object->SetSprite(sprite);
+	object->Render(sm_Direct3D->GetDeviceContext(), x, y);
+	RenderSpritesheetMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+
+//
+//  Abstraction for writing a TexturedSpritesheet to buffer. 
+//  int x, int y: X and Y coords (self explanatory) 
+//  float rot: Clockwise rotation in degrees
+//
+inline bool SceneClass::RenderSpritesheet(TexturedSpritesheet* object, int x, int y, float rot, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix = DirectX::XMMatrixRotationZ(DirectX::XMConvertToRadians(rot)) * DirectX::XMMatrixTranslation(x - 400 + object->m_spriteWidth / 2, -y + 300 - object->m_imageHeight / 2, 0.f);
+	object->Render(sm_Direct3D->GetDeviceContext(), x, y);
+	RenderSpritesheetMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+}
+
+//
+//  Abstraction for writing a TexturedSpritesheet to buffer. 
+//  int x, int y: X and Y coords (self explanatory) 
+//  float rot: Clockwise rotation in degrees
+//  int sprite: Switches sprite number (calls TexturedSpritesheet::SetSprite(sprite))
+//
+inline bool SceneClass::RenderSpritesheet(TexturedSpritesheet* object, int x, int y, float rot, int sprite, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix = DirectX::XMMatrixRotationZ(rot) * DirectX::XMMatrixTranslation(x - 400 + object->m_spriteWidth / 2, -y + 300 - object->m_imageHeight / 2, 0.f);
+	object->SetSprite(sprite);
+	object->Render(sm_Direct3D->GetDeviceContext(), x, y);
+	RenderSpritesheetMatrix(object, matrix, viewMatrix, orthoMatrix, shaderType);
+	return false;
+}
+inline bool SceneClass::RenderModel(ModelClass* object, XMMATRIX viewMatrix, XMMATRIX orthoMatrix, int shaderType) {
+	DirectX::XMMATRIX matrix = DirectX::XMMatrixTranslation(1.f, 1.f, 1.f);
+	object->Render(sm_Direct3D->GetDeviceContext());
+	return sm_ShaderClass->m_TextureShader->Render(sm_Direct3D->GetDeviceContext(), object->GetIndexCount(), matrix, viewMatrix, orthoMatrix, object->GetTexture());
+}
